412f0df32c7f3efde9e9820f7dbb87ce44167fdd
Sohail Saha <captainwoof.official@gmail.com>
2021-11-26 22:46:15 +0530

    Landing page ready

--- src/components/pages/landing/index.tsx
+++ src/components/pages/landing/index.tsx
@@ -1,5 +1,19 @@
+import { handleLogin } from '../../../lib/firebase'
+import Button from '../../atoms/button'
+import Container from '../../atoms/container'
+import styles from './styles.module.css'
+
 export default function LandingPage() {
     return (
-        <></>
+        <Container>
+            <main className={styles.call_to_action_container}>
+                <h1 className={styles.title}>Anchat</h1>
+                <p className={styles.subtitle}>Chat with people, for free.</p>
+                <Button label="Join or login" style={{ marginTop: 'var(--sp-300)' }}
+                    buttonProps={{ onClick: () => { handleLogin() } }}>
+                    Join/Login
+                </Button>
+            </main>
+        </Container>
     )
 }
