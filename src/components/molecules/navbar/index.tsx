07ef98b213f6c0606c9c0f92d4f8931689064863
Sohail Saha <captainwoof.official@gmail.com>
2021-11-26 11:32:23 +0530

    Navbar ready

---
+++ src/components/molecules/navbar/index.tsx
@@ -0,0 +1,45 @@
+import styles from './styles.module.css'
+import { RiWechat2Line as Logo } from 'react-icons/ri'
+import { HiMenu as MenuIcon } from 'react-icons/hi'
+import { Link } from 'react-router-dom'
+import { useState } from 'react'
+import cx from 'classnames'
+import { useUser } from '../../../hooks/user'
+import { handleLogin, handleLogout } from '../../../lib/firebase'
+
+export default function Navbar() {
+    const [showMenu, setShowMenu] = useState<boolean>(false)
+    const { user } = useUser()
+
+    return (
+        <header className={styles.header}>
+            <Link to='' className={styles.title_container}>
+                <h5 className={styles.title}>
+                    <Logo />
+                    Anchat
+                </h5>
+            </Link>
+            <nav className={styles.navbar}>
+                <div className={styles.menu_icon_container}
+                    onClick={() => { setShowMenu(prev => !prev) }}>
+                    <MenuIcon className={styles.menu_icon} />
+                </div>
+                <ul className={cx(styles.menu_items, showMenu ? null : styles.closed)}>
+                    <Link to="about">
+                        <li className={styles.menu_item}>About</li>
+                    </Link>
+
+                    {!!user &&
+                        <Link to="">
+                            <li className={styles.menu_item}>Rooms</li>
+                        </Link>
+                    }
+
+                    <li className={styles.menu_item} onClick={!!user ? handleLogout : handleLogin}>
+                        {!!user ? 'Logout' : 'Login'}
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
